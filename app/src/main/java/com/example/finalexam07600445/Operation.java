747dcc4aeba6d02ed28b42afe53c4f10054b04d8
Chaninat Charoensup <Next640336@gmail.com>
2019-11-17 12:28:53 +0700

    aa

---
+++ app/src/main/java/com/example/finalexam07600445/Operation.java
@@ -0,0 +1,7 @@
+package th.ac.su.sc.finalexam07600659.Operations;
+
+public interface Operation {
+    boolean login(String name, String pass);
+    String ans();
+}
+
--- app/src/main/java/com/example/finalexam07600445/appdatabase.java
+++ app/src/main/java/com/example/finalexam07600445/appdatabase.java
@@ -5,15 +5,13 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-import com.example.finalexam07600445.regisitem;
-
 
 @Database(entities = {regisitem.class}, exportSchema = false, version = 1)
 public abstract class appdatabase extends RoomDatabase {
 
     private static final String DB_NAME = "place.db";
 
-    public abstract placed placeDao();
+    public abstract Operation placeDao();
 
     private static appdatabase mInstance;
 
